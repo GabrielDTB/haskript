b99baaf35212c0cdb1eee2cc5be854c781d15972
Gabriel Talbert Bunt <gabriel@gabrieltb.me>
2025-06-18 05:36:24 -0400

    Simplify

--- flake.nix
+++ flake.nix
@@ -3,38 +3,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {inherit system;};
-        makeEnv = name: packages:
-          pkgs.buildEnv {
-            inherit name;
-            paths =
-              [
-                (pkgs.haskellPackages.ghcWithPackages (_: packages))
-              ]
-              ++ packages;
-            meta.mainProgram = "runghc";
-            passthru =
-              pkgs.lib.genAttrs
-              (builtins.attrNames pkgs.haskellPackages)
-              (
-                pkgName:
-                  makeEnv "${name}-${pkgName}"
-                  (packages ++ [pkgs.haskellPackages.${pkgName}])
-              );
-          };
-      in {
-        legacyPackages =
-          pkgs
-          // {
-            haskellWith = makeEnv "ghc" [];
-          };
-      }
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+      hp = pkgs.haskellPackages;
+      ghcTree = with pkgs.lib; with builtins; pkgSet: (
+        pkgs.buildEnv {
+          name = "haskript";
+          paths = [(hp.ghcWithPackages (_: pkgSet))];
+          meta.mainProgram = "runghc";
+        }
+      ) // genAttrs (attrNames hp) (pkg: ghcTree (pkgSet ++ [hp.${pkg}]));
+    in
+      { packages = ghcTree []; }
     );
 }
